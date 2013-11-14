829a93b21cfa98dbbf0e880a450b327d33f1c3a6
kisaragui <kisa_ridmon@msn.com>
2013-11-14 13:47:18 -0430

    modificado  la capeta base popcon.debian.org a popcon-canima

--- popcon-process.sh
+++ popcon-process.sh
@@ -1,6 +1,6 @@
-bin/sh
+#!/bin/sh
 
-BASEDIR=/srv/popcon.debian.org/popcon-mail
+BASEDIR=/srv/popcon-canaima/popcon-mail
 MAILDIR=../Mail
 WEBDIR=../www
 LOGDIR=$BASEDIR/../logs
@@ -18,7 +18,7 @@ DAYLIMIT=20
 set -e
 cd $BASEDIR
 umask 0002
-
+echo "pasando a /srv/popcon.debian.org/popcon-mail"
 # rotate incoming mail spool files
 if [ true = "$READMAIL" ] ; then
     mv $MAILDIR/survey new-popcon-entries
@@ -27,7 +27,7 @@ if [ true = "$READMAIL" ] ; then
 
     # process entries, splitting them into individual reports
     /var/lib/popcon/bin/prepop.pl <new-popcon-entries >$LOGDIR/prepop.out 2>&1
-    
+    echo "ejecuando prepop.pl"
 fi
 
 # delete outdated entries
@@ -35,28 +35,28 @@ rm -f results results.stable
 find $DATADIR -type f -mtime +$DAYLIMIT -print0 | xargs -0 rm -f --
 
 # Generate statistics
-
+echo " generando estadisticas"
 find $DATADIR -type f | xargs cat \
-        | nice -15 $BINDIR/popanal.py >$LOGDIR/popanal.out 2>&1
-     
+        | nice -15 $BINDIR/popanal.py #>$LOGDIR/popanal.out 2>&1
+     echo "ejecuando popanal.py"
 cp results $WEBDIR/all-popcon-results
-
+echo "copiando results"
 cp results.stable $WEBDIR/stable/stable-popcon-results
 gzip -f $WEBDIR/all-popcon-results
-
+echo " compriendo results en all-popcon-results"
 gzip -f $WEBDIR/stable/stable-popcon-results
 cp $WEBDIR/all-popcon-results.gz $SUMMARYDIR/popcon-`date +"%Y-%m-%d"`.gz
-
+echo "copiendo en all-popcon-results"
 cp $WEBDIR/stable/stable-popcon-results.gz $SUMMARYDIRSTABLE/popcon-`date +"%Y-%m-%d"`.stable.gz
 
-
+echo " pasando a popcon-stat"
 cd ../popcon-stat
-
+echo " ejecutando popcon-stat.pl "
 find $SUMMARYDIR -type f -print | sort | $BINDIR/popcon-stat.pl #>$LOGDIR/popstat.log 
- 
+echo "ejecuando en stable"  
 #esta parte solo es para ejecutar popcon-stat.pl y crear las imagenes.png para la pestaña stable reports, requiere de results.stable y configurarlo 
 #find $SUMMARYDIRSTABLE -type f -print | sort | $BINDIR/popcon-stat.pl ../www/stable/stat >> $LOGDIR/popstat.log 2>&1 
-
+echo " pasando a popcon-web"
 cd ../popcon-web
-
-$BINDIR/popcon.pl >$LOGDIR/popcon.log 2>$LOGDIR/popcon.errors
+echo "ejecutando popcon-web"
+$BINDIR/popcon.pl #>$LOGDIR/popcon.log 2>$LOGDIR/popcon.errors
