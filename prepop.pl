ed53417bdfa110eca8fd23ff183531999a8dc9c5
noel <kisa_ridmon@msn.com>
2013-11-28 11:07:18 -0430

    modificaciones menores: seguimiento del servidor web

--- prepop.pl
+++ prepop.pl
@@ -8,11 +8,19 @@
 $dirname = '/var/lib/popcon/bin/popcon-entries';
 $now = time;
 $state='initial'; # one of ('initial','accept','reject')
+#open(REPORTE2, '>>reporte2.txt');
+#print REPORTE2 "entra a prepop.pl\n";
 
+print "ENTRO A PREPOP.PL\n";
+
+$i = 0;
 
 my($file,$mtime);
 while(<>)
 {
+	#print "$i: $_\n";
+	#$i = $i + 1;
+
     $state eq 'initial' and do
     {
        /^POPULARITY-CONTEST-0/ or next;
@@ -25,30 +33,32 @@ while(<>)
             
        };
        $id=$field{'ID'};
+	print "IDPP: $id\n";
        if (!defined($id) || $id !~ /^([a-f0-9]{32})$/) 
        {
          print STDERR "Bad hostid: $id\n";
-         $state='reject'; next;
+         $state='reject';print "entrando en estado REJECT ID (1)\n"; next;
        }
        $id=$1; #untaint $id
        
        $mtime=$field{'TIME'};
        if (!defined($mtime) || $mtime!~/^([0-9]+)$/)
        {
-         
          print STDERR "Bad mtime $mtime\n";
-         $state='reject'; next;
+         $state='reject';print "entrando en estado REJECT MTIME (2)\n"; next;
        }
        $mtime=int $1; #untaint $mtime;
        $mtime=$now if ($mtime > $now);
        my $dir=substr($id,0,2);
-       unless (-d "$dirname/$dir") {
-	 
-         mkdir("$dirname/$dir",0755) or do {$state='reject';next;};
-         
+	print "DIRPP: $dir\n";
+       unless (-d "$dirname/$dir") { 
+
+	mkdir("$dirname/$dir",0755) or do {$state='reject';print "entrando en estado REJECT no puede crear carpeta (3)\n"; next;}
+;
        };
        $file="$dirname/$dir/$id"; 
-       open REPORT, ">",$file or do {$state='reject';next;};
+	print "FILEPP: $file\n";
+	open REPORT, ">",$file or do {$state='reject';print "entrando en estado REJECT NO copio el informe (4)\n";next;};
        print REPORT $_;
        $state='accept'; next;
     };
@@ -83,3 +93,5 @@ if ($state eq 'accept')
         unlink $file; #Reject
         print STDERR "Bad last report $file\n";
 }
+
+print "SALE DE PREPOP.PL\n";
